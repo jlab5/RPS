2d7ea4ea1d6ae6bd738344b63bc123a8d29de38a
Jed <jedlabarentos@gmail.com>
2023-03-13 12:28:18 -0400

    Feat develop function to play game for 5 rounds

--- rps.js
+++ rps.js
@@ -1,5 +1,7 @@
+const outcomes = ["Rock","Paper","Scissors"];
+const lowerOutcomes = outcomes.map(x => x.toLowerCase());
+
 function getComputerChoice() {
-    const outcomes = ["Rock","Paper","Scissors"];
     //Math.random() generates a random floating point number between 0 and 1
     const randomIndex = Math.floor(Math.random() * outcomes.length);
     const choice = outcomes[randomIndex];
@@ -8,32 +10,60 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    //checks for wrong inputs
+    while (typeof playerSelection !== "string" || lowerOutcomes.includes(playerSelection.toLowerCase()) == false) {
+        playerSelection = prompt("Wrong input. Try Again. What will you choose?");
+    }
     //Converts the first letter to upper case and the rest lower case
     const cleanPlayerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
-
     //WIN cases
     const WIN = (cleanPlayerSelection === "Rock" && computerSelection === "Scissors"
     || cleanPlayerSelection === "Paper" && computerSelection === "Rock" 
     || cleanPlayerSelection === "Scissors" && computerSelection === "Paper")
     //DRAW cases
-    const DRAW = (cleanPlayerSelection === computerSelection)
-    console.log(WIN)
+    const DRAW = (cleanPlayerSelection === computerSelection);
 
     if (WIN == true) {
-        return "You win! " + cleanPlayerSelection +  " beats " + computerSelection; 
+        console.log("You win! " + cleanPlayerSelection +  " beats " + computerSelection)
+        return "won"
     }
     else if (DRAW == true) {
-        return "Draw. No one wins"
+        console.log("Draw. No one wins")
+        return "draw"
     }
     else {
-        return "You lose..." + computerSelection +  " beats " + cleanPlayerSelection; 
+        console.log("You lose..." + computerSelection +  " beats " + cleanPlayerSelection)
+        return "lost"
     }
 }
 
-const playerSelection = "RoCk";
-const computerSelection = getComputerChoice();
-console.log(playerSelection,computerSelection)
-console.log(playRound(playerSelection,computerSelection))
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("What will you choose?");
+        const computerSelection = getComputerChoice();
+        const outcome = playRound(playerSelection, computerSelection);
+        if (outcome == "won") {
+            playerScore += 1;
+        }
+        else if (outcome == "lost") {
+            computerScore += 1;
+        }
+        //else would do nothing
+    }
+    if (playerScore > computerScore) {
+        return ("You won " + playerScore + "-" + computerScore)
+    }
+    else if (playerScore < computerScore) {
+        return ("You lost " + computerScore + "-" + playerScore)
+    }
+    else {
+        return ("You tied " + computerScore + "-" + playerScore)
+    }
+}
+
+console.log(game())
     
 
 
