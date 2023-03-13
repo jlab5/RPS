b1bbe8ac9b20fc9a19e2208b942370c476a203dd
Jed <jedlabarentos@gmail.com>
2023-03-13 11:07:43 -0400

    Feat develop function to play game for one round

--- rps.js
+++ rps.js
@@ -1,12 +1,41 @@
 function getComputerChoice() {
     const outcomes = ["Rock","Paper","Scissors"];
     //Math.random() generates a random floating point number between 0 and 1
-    let randomIndex = Math.floor(Math.random() * outcomes.length);
-    let choice = outcomes[randomIndex];s
+    const randomIndex = Math.floor(Math.random() * outcomes.length);
+    const choice = outcomes[randomIndex];
     return choice;
 
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    //Converts the first letter to upper case and the rest lower case
+    const cleanPlayerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
 
+    //WIN cases
+    const WIN = (cleanPlayerSelection === "Rock" && computerSelection === "Scissors"
+    || cleanPlayerSelection === "Paper" && computerSelection === "Rock" 
+    || cleanPlayerSelection === "Scissors" && computerSelection === "Paper")
+    //DRAW cases
+    const DRAW = (cleanPlayerSelection === computerSelection)
+    console.log(WIN)
+
+    if (WIN == true) {
+        return "You win! " + cleanPlayerSelection +  " beats " + computerSelection; 
+    }
+    else if (DRAW == true) {
+        return "Draw. No one wins"
+    }
+    else {
+        return "You lose..." + computerSelection +  " beats " + cleanPlayerSelection; 
+    }
+}
+
+const playerSelection = "RoCk";
+const computerSelection = getComputerChoice();
+console.log(playerSelection,computerSelection)
+console.log(playRound(playerSelection,computerSelection))
+    
+
+
+//Shows message through a button
 // document.getElementById("myBtn").onclick = getComputerChoice;
