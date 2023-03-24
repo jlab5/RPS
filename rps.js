4b030d478bebc1dacf046c2072b7993cfe911db9
Jed <jedlabarentos@gmail.com>
2023-03-24 10:04:18 -0400

    Refactor: Modify functions to enable compatibility with HTML and CSS

--- rps.js
+++ rps.js
@@ -1,71 +1,58 @@
-const outcomes = ["Rock","Paper","Scissors"];
-const lowerOutcomes = outcomes.map(x => x.toLowerCase());
+const OUTCOMES = ["Rock","Paper","Scissors"];
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('button');
 
 function getComputerChoice() {
     //Math.random() generates a random floating point number between 0 and 1
-    const randomIndex = Math.floor(Math.random() * outcomes.length);
-    const choice = outcomes[randomIndex];
+    const randomIndex = Math.floor(Math.random() * OUTCOMES.length);
+    const choice = OUTCOMES[randomIndex];
     return choice;
+}
 
+function choices(playerChoice,getComputerChoice) {
+    return [playerChoice,getComputerChoice];
 }
 
 function playRound(playerSelection, computerSelection) {
-    //checks for wrong inputs
-    while (typeof playerSelection !== "string" || lowerOutcomes.includes(playerSelection.toLowerCase()) == false) {
-        playerSelection = prompt("Wrong input. Try Again. What will you choose?");
-    }
-    //Converts the first letter to upper case and the rest lower case
-    const cleanPlayerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+    console.log(playerSelection,computerSelection);
     //WIN cases
-    const WIN = (cleanPlayerSelection === "Rock" && computerSelection === "Scissors"
-    || cleanPlayerSelection === "Paper" && computerSelection === "Rock" 
-    || cleanPlayerSelection === "Scissors" && computerSelection === "Paper")
+    const WIN = (playerSelection === "Rock" && computerSelection === "Scissors"
+    || playerSelection === "Paper" && computerSelection === "Rock" 
+    || playerSelection === "Scissors" && computerSelection === "Paper")
     //DRAW cases
-    const DRAW = (cleanPlayerSelection === computerSelection);
+    const DRAW = (playerSelection === computerSelection);
 
     if (WIN == true) {
-        console.log("You win! " + cleanPlayerSelection +  " beats " + computerSelection)
-        return "won"
+        console.log("You win! " + playerSelection +  " beats " + computerSelection)
+        playerScore += 1;
     }
     else if (DRAW == true) {
-        console.log("Draw. No one wins")
-        return "draw"
+        console.log("Draw. No one wins");
     }
     else {
-        console.log("You lose..." + computerSelection +  " beats " + cleanPlayerSelection)
-        return "lost"
+        console.log("You lose..." + computerSelection +  " beats " + playerSelection);
+        computerScore += 1;
     }
+    console.log(`Your score: ${playerScore} \nComputer Score: ${computerScore}`)
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("What will you choose?");
-        const computerSelection = getComputerChoice();
-        const outcome = playRound(playerSelection, computerSelection);
-        if (outcome == "won") {
-            playerScore += 1;
-        }
-        else if (outcome == "lost") {
-            computerScore += 1;
-        }
-        //else would do nothing
-    }
-    if (playerScore > computerScore) {
-        return ("You won " + playerScore + "-" + computerScore)
-    }
-    else if (playerScore < computerScore) {
-        return ("You lost " + computerScore + "-" + playerScore)
-    }
-    else {
-        return ("You tied " + computerScore + "-" + playerScore)
-    }
+function game() {  
+    buttons.forEach((button) => {
+        let playerChoice;
+        button.addEventListener('click', (e)=> {
+            if (button.className === "R") {
+                playerChoice = "Rock";
+            }
+            else if (button.className === "P") {
+                playerChoice = "Paper";
+            }
+            else {
+                playerChoice = "Scissors";
+            }
+            playRound(playerChoice, getComputerChoice());
+        })
+    })
 }
 
-console.log(game())
-    
-
-
-//Shows message through a button
-// document.getElementById("myBtn").onclick = getComputerChoice;
+game();
